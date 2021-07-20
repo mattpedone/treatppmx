11d7ab1310f3db8a1b5f56c6822b31ca6868a017
mattpedone <mattpedone7@gmail.com>
2021-07-20 14:05:03 +0200

    documenting scenario2

---
+++ R/SimuOutsce2.R
@@ -0,0 +1,12 @@
+#' Simulation Study 1 Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the 91 predictive covariates and 2 prognostic covariates
+#' for 152 patients assigned to two different treatments.
+#'
+#' @format A data frame with 152 rows and 91 variables:
+#' \describe{
+#'   \item{cov 1-90}{predictive}
+#'   \item{cov 91-92}{prognostic}
+#' }
+# @source \url{http://www.diamondse.info/}
+"simstud1"
